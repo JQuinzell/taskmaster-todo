a0ad795c4055007c430f881c2a6f66e462fb37b5
jquinzell <>
2025-10-09 12:43:54 -0500

    setup app shell

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,39 @@
 import { useQuery } from 'convex/react'
 import { api } from '../convex/_generated/api'
-import './App.css'
+import { SidebarInset, SidebarProvider } from './components/ui/sidebar'
+import { AppSidebar } from './components/app-sidebar'
+import { SiteHeader } from './components/site-header'
 
 function App() {
   const tasks = useQuery(api.tasks.get)
 
-  return tasks?.map((task) => (
-    <div key={task._id}>
-      <p>{task.text}</p>
-      <p>{task.status}</p>
-    </div>
-  ))
+  return (
+    <SidebarProvider
+      style={
+        {
+          '--sidebar-width': 'calc(var(--spacing) * 72)',
+          '--header-height': 'calc(var(--spacing) * 12)',
+        } as React.CSSProperties
+      }
+    >
+      <AppSidebar variant='inset' />
+      <SidebarInset>
+        <SiteHeader />
+        <div className='flex flex-1 flex-col'>
+          <div className='@container/main flex flex-1 flex-col gap-2'>
+            <div className='flex flex-col gap-4 py-4 md:gap-6 md:py-6'>
+              {tasks?.map((task) => (
+                <div key={task._id} className='flex gap-2'>
+                  <div className='flex-1'>{task.text}</div>
+                  <div className='flex-none'>{task.status}</div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  )
 }
 
 export default App
