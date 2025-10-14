be7c6af86d1e512c8fd4931a573f30be749aebef
jquinzell <>
2025-10-14 14:55:56 -0500

    show date

--- src/TaskList.tsx
+++ src/TaskList.tsx
@@ -5,6 +5,8 @@ import { Separator } from './components/ui/separator'
 import { useTasks } from './TaskProvider'
 import { useMutation } from 'convex/react'
 import { api } from '../convex/_generated/api'
+import { Badge } from './components/ui/badge'
+import { intlFormatDistance } from 'date-fns'
 
 export function TaskList() {
   const { tasks } = useTasks()
@@ -15,7 +17,7 @@ export function TaskList() {
       <Separator />
       {tasks.map((task) => (
         <Fragment key={task._id}>
-          <Label className='flex gap-2 items-center p-2'>
+          <div className='flex gap-2 items-center p-2'>
             <Checkbox
               id={task._id}
               checked={task.status === 'completed'}
@@ -27,8 +29,15 @@ export function TaskList() {
                 })
               }
             />
-            <p>{task.text}</p>
-          </Label>
+            <Label htmlFor={task._id}>
+              <p>{task.text}</p>
+            </Label>
+            {task.dueDate && (
+              <Badge variant='secondary' className='ml-auto'>
+                {intlFormatDistance(task.dueDate, new Date())}
+              </Badge>
+            )}
+          </div>
           <Separator />
         </Fragment>
       ))}
